5c5395154955a8da1db7abcc00d7b20f9fcee5d0
Luís Martinho <lmartinho@gmail.com>
2016-08-20 07:34:33 +0200

    Add the Env class which models a Lisp environment 

--- luisp.py
+++ luisp.py
@@ -4,6 +4,17 @@ import traceback
 Symbol = str
 isa = isinstance
 
+class Env(dict):
+    "An environment: a dict of {'var': val} pairs with an outer Env."
+
+    def __init__(self, params=(), args=(), outer=None):
+        self.update(zip(params, args))
+        self.outer = outer
+
+    def find(self, var):
+        "Find the innermost Env where var appears."
+        return self if var in self else self.outer.find(var)
+
 def parse(s):
     "Parse a Luisp expression from a string."
     return read_from(tokenize(s))
